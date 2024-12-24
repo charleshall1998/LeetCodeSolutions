a360b0f5df9b980e0c30dcf3c4661410d8a82c4d
charleshall1998 <charleskhanhhall@gmail.com>
2024-12-24 12:09:59 -0500

    Added RomanToInteger.

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,11 @@
+﻿using RomanToInteger;
+
+/**
+ * Problem Statement:
+ * Given a roman numeral, convert it to an integer.
+ */
+var romanNumeral = "IV";
+
+var solution = new Solution();
+
+Console.WriteLine(solution.RomanToInt(romanNumeral));
---
+++ RomanToInteger/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace RomanToInteger
+{
+    public class Solution
+    {
+        /**
+         * Psuedo Code:
+         * 1) Create a dictionary to map roman characters to integers
+         * 2) Initialize a variable to store the result
+         * 3) Iterate through the string
+         * 4) If the current character is less than the next character, subtract the current character from the result
+         * 5) Otherwise, add the current character to the result
+         * 6) Return the result
+         */
+        public int RomanToInt(string s)
+        {
+            Dictionary<char, int> romanToInt = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+
+            var toReturn = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i != s.Length - 1 && romanToInt[s[i + 1]] > romanToInt[s[i]])
+                {
+                    toReturn -= romanToInt[s[i]];
+                }
+                else
+                {
+                    toReturn += romanToInt[s[i]];
+                }
+            }
+
+            return toReturn;
+        }
+    }
+}
