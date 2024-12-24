aacbc1d1790c28dc0fe837c511c1e68beeecf75b
charleshall1998 <charleskhanhhall@gmail.com>
2024-12-24 14:42:54 -0500

    Added LCP.

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,16 @@
+﻿using LongestCommonPrefix;
+
+/**
+ * Problem Statement:
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ */
+var strs = new string[] { "flower", "flow", "flight" };
+
+//Correct Answer: "fl"
+
+var solution = new Solution();
+
+var result = solution.LongestCommonPrefix(strs);
+
+Console.WriteLine(result);
---
+++ LongestCommonPrefix/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LongestCommonPrefix
+{
+    public class Solution
+    {
+        /**
+         * Pseudo Code:
+         * 1) Set the first string in the array to the longest common prefix (LCP)
+         * 2) Iterate over `strs`
+         * 3) Check if LCP is contained in the string
+         * 4) If yes, LCP will remain the same. Continue iteration.
+         * 5) Else, remove the last letter from LCP until true.
+         */
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var lcp = strs[0];
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(lcp) != 0)
+                {
+                    lcp = lcp.Substring(0, lcp.Length - 1);
+                }
+            }
+
+            return lcp;
+        }
+    }
+}
