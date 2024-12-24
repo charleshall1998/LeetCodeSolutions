778cb9b5b8e3915a13afc44aacb47e1d00091c86
charleshall1998 <charleskhanhhall@gmail.com>
2024-12-24 11:41:52 -0500

    Add project files.

---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,15 @@
+﻿using Palindrome_Number;
+
+/**
+ * Problem Statement:
+ * 
+ * Given an integer `x`, return `true` if `x` is a palindrome, and `false` otherwise.
+ * An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+ */
+var x = 121;
+
+var solution = new Solution();
+
+var result = solution.IsPalindrome(x);
+
+Console.WriteLine(result);
---
+++ LeetCodeSolutions/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace Palindrome_Number
+{
+    public class Solution
+    {
+
+        /**
+         * Pseudo Code:
+         * 1) Create two pointers, one that starts from the beginng & another that starts from the end.
+         * 2) Iterate over the number from the beginning and the end.
+         * 3) If the characters at the two pointers are not equal, return false.
+         * 4) Else, increment the first pointer and decrement the second pointer.
+         * 5) If the loop completes, return true.
+         */
+        public bool IsPalindrome(int x)
+        {
+            var numString = x.ToString();
+            var p1 = 0;
+            var p2 = numString.Length - 1;
+
+            while (p1 != p2 && p1 < numString.Length / 2)
+            {
+                if (numString[p1] != numString[p2])
+                {
+                    return false;
+                }
+
+                p1++;
+                p2--;
+            }
+
+            return true;
+        }
+    }
+}
