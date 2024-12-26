752d95f465f9074be3d5b07171c58e540cbc9bdf
charleshall1998 <charleskhanhhall@gmail.com>
2024-12-26 14:58:17 -0500

    Added RemoveDuplicatesFromSortedArray.

---
+++ RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using RemoveDuplicatesFromSortedArray;
+
+/**
+ * Problem Statement:
+ * Given an integer array `nums` sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+ * The relative order of the elements should be kept the same.
+ * Then return the number of unique elements in nums.
+ */
+var nums = new int[] { 1, 1, 2 };
+
+var solution = new Solution();
+
+var result = solution.RemoveDuplicates(nums);
+
+Console.WriteLine(result); // Output: 2
---
+++ RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        /**
+         * Pseudo Code:
+         * 1) Initialize left pointer
+         * 2) Iterate over the array with right pointer
+         * 3) If the current element is not equal to the previous element, then update the left pointer
+         * 4) Update the left pointer with the current element
+         * 5) Return the left pointer
+         */
+        public int RemoveDuplicates(int[] nums)
+        {
+            int l = 1;
+
+            for (int r = 1; r < nums.Length; r++)
+            {
+                if (nums[r] != nums[r - 1])
+                {
+                    nums[l] = nums[r];
+                    l++;
+                }
+            }
+
+            return l;
+        }
+    }
+}
