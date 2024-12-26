112ddbe9ff0066a5717d03e8503111b27d9f0af7
charleshall1998 <charleskhanhhall@gmail.com>
2024-12-26 15:03:01 -0500

    Added RemoveElement.

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Given an integer array `nums` and an integer `val`, remove all occurrences of `val` in `nums` in-place.
+ * The order of the elements may be changed.
+ * Then return the number of elements in `nums` which are not equal to `val`.
+ */
+
+using RemoveElement;
+
+var nums = new int[] { 3, 2, 2, 3 };
+
+var val = 3;
+
+var solution = new Solution();
+
+var result = solution.RemoveElement(nums, val);
+
+Console.WriteLine(result); // Output: 2
---
+++ RemoveElement/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace RemoveElement
+{
+    public class Solution
+    {
+        /**
+         * 1) Initialize a variable to keep track of the non-val index
+         * 2) Iterate over the array
+         * 3) If the current element is not equal to the val, then update the non-val index
+         * 4) Update the non-val index with the current element
+         * 5) Return the non-val index
+         */
+        public int RemoveElement(int[] nums, int val)
+        {
+            var nonValIndex = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[nonValIndex] = nums[i];
+                    nonValIndex++;
+                }
+            }
+
+            return nonValIndex;
+        }
+    }
+}
