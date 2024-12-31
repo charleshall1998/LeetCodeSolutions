2d4dd1d69f7eae9e083dd274520686e5615d3ea3
charleshall1998 <charleskhanhhall@gmail.com>
2024-12-31 16:20:01 -0500

    Added Merge Sorted Array.

---
+++ MergeSortedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using MergeSortedArray;
+
+/*
+ * You are given two integer arrays `nums1` and `nums2`, sorted in non-decreasing order, and two integers `m` and `n`, representing the number of elements in `nums1` and `nums2` respectively.
+ * Merge `nums1` and `nums2` into a single array sorted in non-decreasing order.
+ * The final sorted array should not be returned by the function, but instead be stored inside the array `nums1`.
+ * To accommodate this, `nums1` has a length of `m` + `n`, where the first `m` elements denote the elements that should be merged, and the last `n` elements are set to 0 and should be ignored.
+ * `nums2` has a length of `n`.
+ */
+
+var nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+
+var nums2 = new int[] { 2, 5, 6 };
+
+var m = 3;
+
+var n = 3;
+
+var solution = new Solution();
+
+solution.Merge(nums1, m, nums2, n);
+
+foreach (var num in nums1)
+{
+    Console.Write(num + " "); // Output: 1 2 2 3 5 6
+}
---
+++ MergeSortedArray/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace MergeSortedArray
+{
+    public class Solution
+    {
+        /*
+         * Psuedo code:
+         * 1) Initialize a variable `lastElement` to `m` + `n` - 1
+         * 2) Loop while `m` and `n` are greater than 0
+         * 3) If the element at `nums1[m - 1]` is greater than the element at `nums2[n - 1]`, set the element at `nums1[lastElement]` to `nums1[m - 1]` and decrement `m`
+         * 4) Otherwise, set the element at `nums1[lastElement]` to `nums2[n - 1]` and decrement `n`
+         * 5) Decrement `lastElement`
+         * 6) Loop while `n` is greater than 0
+         * 7) Set the element at `nums1[lastElement]` to `nums2[n - 1]` and decrement `n` and `lastElement`
+         */
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var lastElement = m + n - 1;
+
+            while (m > 0 && n > 0)
+            {
+                if (nums1[m - 1] > nums2[n - 1])
+                {
+                    nums1[lastElement] = nums1[m - 1];
+                    m--;
+                }
+                else
+                {
+                    nums1[lastElement] = nums2[n - 1];
+                    n--;
+                }
+
+                lastElement--;
+            }
+
+            while (n > 0)
+            {
+                nums1[lastElement] = nums2[n - 1];
+                n--;
+                lastElement--;
+            }
+        }
+    }
+}
