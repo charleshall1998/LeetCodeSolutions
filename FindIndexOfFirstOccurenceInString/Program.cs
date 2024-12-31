2622499728284a1f001ab059e5ab71ee55f5a397
charleshall1998 <charleskhanhhall@gmail.com>
2024-12-31 16:04:17 -0500

    Added Find the Index of the First Occurrence in a String.

---
+++ FindIndexOfFirstOccurenceInString/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Problem Statement:
+ * Given two strings `needle` and `haystack`, return the index of the first occurrence of `needle` in `haystack`, or -1 if `needle` is not part of `haystack`.
+ */
+
+using FindIndexOfFirstOccurenceInString;
+
+var haystack = "sadbutsad";
+
+var needle = "sad";
+
+var solution = new Solution();
+
+var result = solution.StrStr(haystack, needle);
+
+Console.WriteLine(result); // Output: 0
---
+++ FindIndexOfFirstOccurenceInString/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace FindIndexOfFirstOccurenceInString
+{
+    public class Solution
+    {
+        /*
+         * Psuedo code:
+         * 1) Loop through the haystack string from 0 to haystack.Length + 1 - needle.Length
+         * 2) Loop through the needle string
+         * 3) If the character at the current index of the haystack string is not equal to the character at the current index of the needle string, break
+         * 4) If the loop completes, return the current index of the haystack string
+         * 5) If the loop completes and no match is found, return -1
+         */
+        public int StrStr(string haystack, string needle)
+        {
+            for (int i = 0; i < haystack.Length + 1 - needle.Length; i++)
+            {
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+
+                    if (j == needle.Length - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
