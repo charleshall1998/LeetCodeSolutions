b01126734855c52629fab41cced5e82594706d5b
charleshall1998 <charleskhanhhall@gmail.com>
2025-01-01 13:46:31 -0500

    Remove redundant comments.

--- SameTree/Solution.cs
+++ SameTree/Solution.cs
@@ -10,25 +10,21 @@
          */
         public bool IsSameTree(TreeNode p, TreeNode q)
         {
-            // Base case: If both trees are empty, they are identical.
             if (p == null && q == null)
             {
                 return true;
             }
 
-            // If one of the trees is empty and the other is not, they are not identical.
             if (p == null || q == null)
             {
                 return false;
             }
 
-            // Compare the values of the current nodes.
             if (p.val != q.val)
             {
                 return false;
             }
 
-            // Recursively check the left and right subtrees.
             return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
         }
     }
