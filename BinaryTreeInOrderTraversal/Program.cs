e7cf2e8fd23e7aede854deed43de1cfe9865c539
charleshall1998 <charleskhanhhall@gmail.com>
2025-01-01 13:36:29 -0500

    Added BinaryTreeInOrderTraversal.

---
+++ BinaryTreeInOrderTraversal/Program.cs
@@ -0,0 +1,20 @@
+﻿using BinaryTreeInOrderTraversal;
+
+/*
+ * Problem Statement:
+ * Given the `root` of a binary tree, return the inorder traversal of its nodes' values.
+ */
+
+var root = new TreeNode(1)
+{
+    right = new TreeNode(2)
+    {
+        left = new TreeNode(3)
+    }
+};
+
+var solution = new Solution();
+
+var result = solution.InorderTraversal(root);
+
+Console.WriteLine($"[{string.Join(", ", result)}]"); // [1, 3, 2]
---
+++ BinaryTreeInOrderTraversal/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace BinaryTreeInOrderTraversal
+{
+    public class Solution
+    {
+        /*
+         * Pseudo Code:
+         * 1) Create a list to store the result.
+         * 2) Create a helper function that takes a TreeNode as an argument.
+         * 3) If the TreeNode is not null, recursively call the helper function with the left child of the TreeNode.
+         * 4) Add the value of the TreeNode to the result list.
+         * 5) Recursively call the helper function with the right child of the TreeNode.
+         * 6) Return the result list.
+         */
+
+        List<int> result = new List<int>();
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            Helper(root);
+            return result;
+        }
+
+        public void Helper(TreeNode root)
+        {
+            if (root != null)
+            {
+                Helper(root.left);
+                result.Add(root.val);
+                Helper(root.right);
+            }
+        }
+    }
+}
